3e2aaff71c5c0c39702993ea6e8142cbf5e15f6c
arnameti <arnameti@gmail.com>
2022-01-14 14:23:40 +0100

    defining the selectors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const btnAgain = document.querySelector('.again');
+const btnCheck = document.querySelector('.check');
+
+const labelMessage = document.querySelector('.message');
+const labelScore = document.querySelector('.label-score');
+const labelHighscore = document.querySelector('.label-highscore');
+
+const inputGuess = document.querySelector('.guess');
+
