0015e20dd5adf8d9201885526207acef55318a76
arnameti <arnameti@gmail.com>
2022-01-18 19:23:13 +0100

    adding addEventListener

--- script.js
+++ script.js
@@ -2,10 +2,33 @@
 
 const btnAgain = document.querySelector('.again');
 const btnCheck = document.querySelector('.check');
-
 const labelMessage = document.querySelector('.message');
-const labelScore = document.querySelector('.label-score');
-const labelHighscore = document.querySelector('.label-highscore');
-
+const labelScore = document.querySelector('.score');
+const labelHighscore = document.querySelector('.highscore');
 const inputGuess = document.querySelector('.guess');
 
+const body = document.querySelector('body');
+const labelNumber = document.querySelector('.number');
+
+let score = 20;
+let highscore;
+let randomNumber = Math.floor(Math.random() * 20) + 1;
+
+labelNumber.textContent = randomNumber;
+
+btnCheck.addEventListener('click', function () {
+  if (Number(inputGuess.value) > randomNumber) {
+    score--;
+    labelMessage.textContent = 'Number too high!';
+    if (score < 1) score = 0;
+    labelScore.textContent = score;
+
+  } else if (Number(inputGuess.value) < randomNumber) {
+    score--;
+    labelMessage.textContent = 'Number too low!';
+    if (score < 1) score = 0;
+    labelScore.textContent = score;
+  } else {
+    labelMessage.textContent = 'You won the game!';
+  }
+});
